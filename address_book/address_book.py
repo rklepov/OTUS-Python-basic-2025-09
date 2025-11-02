6f71310472b91ee666cfa0ebcb3be8e84b6071f7
Roman Klepov <me@rklepov.ru>
2025-11-03 01:25:00 +0300

    feat(hw01): implement the main menu and loop

--- address_book/address_book.py
+++ address_book/address_book.py
@@ -1,9 +1,9 @@
 # address_book/address_book.py
 
 import json
+from operator import itemgetter
 from pathlib import Path
 from typing import Any
-
 
 type Id = int
 type Contact = dict[str, Any]
@@ -29,10 +29,11 @@ def save_contacts(file_path: Path, address_book: AddressBook) -> None:
 
 def show_contacts(address_book: AddressBook) -> None:
     print(f'Контакты ({len(address_book)}):')
-    print()
+    print('-' * 80)
     for id, contact in address_book.items():
-        print_contact(id, contact)
         print('-' * 80)
+        print_contact(id, contact)
+    print('-' * 80)
 
 
 def create_contact(
@@ -65,6 +66,129 @@ def update_contact(
 def delete_contact(address_book: AddressBook, id: Id) -> tuple[Contact, AddressBook]:
     deleted_contact: Contact = address_book.pop(id)
     return (deleted_contact, address_book)
+
+
+def prompt_selection(menu: list[str, Any]) -> Any:
+    for n, (prompt, _) in enumerate(menu):
+        print(f'({n + 1}): {prompt}')
+
+    print()
+
+    action = None
+
+    if (selection := input('> ').strip()).isdigit():
+        if 0 < (item := int(selection)) <= len(menu):
+            _, action = menu[item - 1]
+
+    return action
+
+
+def command_open(*, file_path: Path, **kwargs):
+    return {
+        **kwargs,
+        'file_path': file_path,
+        'dirty_flag': False,
+    }
+
+
+def command_save(*, address_book: AddressBook, file_path: Path, **kwargs):
+    return {
+        **kwargs,
+        'address_book': address_book,
+        'file_path': file_path,
+        'dirty_flag': False,
+    }
+
+
+def command_show_contacts(*, address_book: AddressBook, **kwargs):
+    show_contacts(address_book)
+    return {
+        **kwargs,
+        'address_book': address_book,
+    }
+
+
+def command_create_contact(*, address_book: AddressBook, **kwargs):
+    return {
+        **kwargs,
+        'address_book': address_book,
+        'dirty_flag': True,
+    }
+
+
+def command_find_contact(*, address_book: AddressBook, **kwargs):
+    return {**kwargs, 'address_book': address_book}
+
+
+def command_update_contact(*, address_book: AddressBook, **kwargs):
+    raise ValueError('zys')
+    return {
+        **kwargs,
+        'address_book': address_book,
+        'dirty_flag': True,
+    }
+
+
+def command_delete_contact(*, address_book: AddressBook, **kwargs):
+    return {
+        **kwargs,
+        'address_book': address_book,
+        'dirty_flag': True,
+    }
+
+
+def command_exit(**kwargs):
+    return {**kwargs, 'stop': True}
+
+
+def main_menu():
+    main_menu = [
+        ('Открыть из файла', command_open),
+        ('Сохранить в файл', command_save),
+        ('Показать все контакты', command_show_contacts),
+        ('Создать контакт', command_create_contact),
+        ('Найти контакт', command_find_contact),
+        ('Изменить контакт', command_update_contact),
+        ('Удалить контакт', command_delete_contact),
+        ('Выход', command_exit),
+    ]
+
+    print('Телефонный справочник')
+    print('---------------------')
+
+    command = prompt_selection(main_menu)
+
+    return command
+
+
+def main():
+    address_book: AddressBook = {}
+    file_path: str = Path(__file__).resolve().parent / f'{Path(__file__).stem}.json'
+    dirty_flag = False
+
+    while True:
+        command = main_menu()
+        print()
+
+        kwargs = {
+            'address_book': address_book,
+            'file_path': file_path,
+            'dirty_flag': dirty_flag,
+            'stop': False,
+        }
+
+        if command:
+            address_book, file_path, dirty_flag, stop = itemgetter(
+                'address_book', 'file_path', 'dirty_flag', 'stop'
+            )(command(**kwargs))
+
+            if stop:
+                print('Bye!')
+                break
+
+
+if __name__ == '__main__':
+    main()
 
 
 # __EOF__
